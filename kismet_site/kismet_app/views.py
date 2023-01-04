16ed9485d9577bac80ff05a963eb03c0407292c7
Milad Moulayi <m.moulayi123@gmail.com>
2023-01-03 17:41:51 -0800

    Created character model, imported to view, and created html boilerplate

--- kismet_site/kismet_app/views.py
+++ kismet_site/kismet_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.views import View
+from kismet_app.models import Character
 
 # Create your views here.
 
 class HomeView(View):
   def get(self, request):
-    return render
+    characters = Character.objects.all()
+
+    html_data = {
+      'character_list': characters,
+    }
+
+    return render(
+        request=request,
+        template_name='index.html',
+        context=html_data,
+    )
