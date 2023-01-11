247d2d4457bf78412a975b52e38a871ad6b7665e
Milad Moulayi <m.moulayi123@gmail.com>
2023-01-10 16:01:45 -0800

    Created Character, Scenario, Choice, and Outcome models, forms, and views

--- kismet_site/kismet_app/forms.py
+++ kismet_site/kismet_app/forms.py
@@ -1,7 +1,24 @@
 from django.forms import ModelForm
-from kismet_app.models import Character
+from kismet_app.models import Character, Scenario, Choice, Outcome
 
 class CharacterForm(ModelForm):
   class Meta:
     model = Character
-    fields = ['name']
+    fields = ['name','alignment']
+
+class ScenarioForm(ModelForm):
+  class Meta:
+    model = Scenario
+    fields = ['name', 'description']
+
+class ChoiceForm(ModelForm):
+  class Meta:
+    model = Choice
+    fields = ['name', 'type']
+
+class OutcomeForm(ModelForm):
+  class Meta:
+    model = Outcome
+    fields = ['name', 'type']
+
+
--- kismet_site/kismet_app/views.py
+++ kismet_site/kismet_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from kismet_app.forms import CharacterForm
-from kismet_app.models import Character
-
+from kismet_app.forms import CharacterForm, ScenarioForm, ChoiceForm, OutcomeForm
+from kismet_app.models import Character, Scenario, Choice, Outcome
 # Create your views here.
 
 class HomeView(View):
@@ -31,6 +30,34 @@ class HomeView(View):
 class CharacterDetailView(View):
   def get(self, request, character_id):
     character=Character.objects.get(id=character_id)
+
+class GameplayView(View):
+  def get(self, request):
+    character_form = CharacterForm()
+    characters = Character.objects.all()
+    choice_form = ChoiceForm()
+    choices = Choice.objects.all()
+    outcome_form = OutcomeForm()
+    outcomes = Outcome.objects.all()
+    scenario_form = ScenarioForm()
+    scenarios = Scenario.objects.all()
+
+    html_data = {
+      'character_list': characters,
+      'character_form': character_form,
+      'choice_list': choices,
+      'choice_form': choice_form,
+      'outcome_list': outcomes,
+      'outcome_form': outcome_form,
+      'scenario_list': scenarios,
+      'scenario_form': scenario_form
+    }
+
+    return render(
+        request=request,
+        template_name='index.html',
+        context=html_data,
+    )
 
 
 
